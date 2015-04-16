f7f0a4acde0e4edbe883575031ed2b2a6830e3f7
Bruno Eduardo <bruno@beduardo.com>
2015-04-15 23:17:50 -0300

    start

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function( grunt ) {
+ 
+  grunt.initConfig({
+    // Tasks que o Grunt deve executar
+        sass : {
+      dist : {
+        options : { style : 'compressed' },
+        files : {
+          'css/style.css' : 'css/_sass/style.scss'
+        }
+      }
+    }, // sass   
+
+        watch : {
+      dist : {
+        files : [
+          'css/_sass/*'
+        ],
+
+        tasks : [ 'sass']
+      }
+    } // watch
+
+  });
+
+    grunt.loadNpmTasks( 'grunt-contrib-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+	// Tarefas que serão executadas
+  grunt.registerTask( 'default', [ 'sass' ] );
+	grunt.registerTask( 'w', [ 'watch' ] );
+};
